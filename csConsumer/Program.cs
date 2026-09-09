83b5f850b5c99a67fbd666ba991fb9634f06978d
mordy <mordypassover@gmail.com>
2026-09-09 10:39:37 +0300

    added cs consumer

---
+++ csConsumer/Program.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using csConsumer.Models;
+using csConsumer.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("Appsettings.json")
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddSingleton<FilterIncommingReportService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var validator = serviceProvider
+    .GetRequiredService<FilterIncommingReportService>();
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = configuration["Kafka:BootstrapServers"],
+    GroupId = configuration["Kafka:GruopId"],
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe(configuration["Kafka:Topics:Raw-data"]);
+var cnt = 0;
+while (true)
+{
+    try
+    {
+        var consumeResult = consumer.Consume();
+
+        string rawJsonReport = consumeResult.Message.Value;
+
+        Report? report;
+
+        try
+        {
+            report = JsonSerializer.Deserialize<Report>(rawJsonReport);
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine($"Invalid JSON: {ex.Message}");
+            continue;
+        }
+
+        if (report == null)
+        {
+            Console.WriteLine("Report is null.");
+            continue;
+        }
+
+        if (validator.Validate(report))
+        {
+            cnt ++;
+            Console.WriteLine($"Valid report: {report.ReportId}");
+
+            //add send to elastic!!!!
+        }
+        else
+        {
+            Console.WriteLine($"Invalid report: {report.ReportId}");
+        }
+    }
+    catch (ConsumeException ex)
+    {
+        Console.WriteLine($"Kafka error: {ex.Error.Reason}");
+    }
+}
---
+++ csConsumer/Services/FilterIncommingReportService.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using csConsumer.Models;
+
+namespace csConsumer.Services;
+
+internal class FilterIncommingReportService
+{
+    private readonly List<string> _usedIds = new();
+
+    public bool Validate(Report report)
+    {
+        var context = new ValidationContext(report);
+        var validationResults = new List<ValidationResult>();
+
+        bool isValid = Validator.TryValidateObject(
+            report,
+            context,
+            validationResults,
+            validateAllProperties: true
+        );
+
+        if (!isValid)
+        {
+            foreach (var result in validationResults)
+            {
+                Console.WriteLine($"Validation error: {result.ErrorMessage}");
+            }
+
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(report.ReportId)|| string.IsNullOrWhiteSpace(report.Unit)||
+            string.IsNullOrWhiteSpace(report.AgentId) || string.IsNullOrWhiteSpace(report.Theater) ||
+            string.IsNullOrWhiteSpace(report.Sector) || string.IsNullOrWhiteSpace(report.Location) ||
+            string.IsNullOrWhiteSpace(report.ReportType) || string.IsNullOrWhiteSpace(report.Priority) ||
+            string.IsNullOrWhiteSpace(report.SourceType) || string.IsNullOrWhiteSpace(report.Message))
+        {
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(report.SubjectId) !=
+            string.IsNullOrWhiteSpace(report.SubjectType))
+        {
+            return false;
+        }
+
+        if (_usedIds.Contains(report.ReportId))
+        {
+            Console.WriteLine($"Duplicate ReportId: {report.ReportId}");
+            return false;
+        }
+
+        _usedIds.Add(report.ReportId);
+
+        return true;
+    }
+}
